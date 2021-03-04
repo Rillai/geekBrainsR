9ddc03d77483ae10ad9dfe0c6262d348fb987ff4
Rillai <62833808+Rillai@users.noreply.github.com>
2021-03-05 00:16:00 +0400

    Upp

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
-import './App.css';
-import { useEffect, useState } from 'react';
-import { Message } from './components/message';
-import { NessageField } from './components/messageField';
+import './App.css'
+import { useCallback, useEffect, useState } from 'react'
+import { Message } from './components/message'
+import { NessageField } from './components/messageField'
+import { usePrev } from './hooks/usePrev'
 
 const App = () => {
-  const [messages, setMessages] = useState([{ id: 0, message: 'dobri den', author: 'me' }, { id: 1, message: 'Privet', author: 'me' }, { id: 2, message: 'React curse', author: 'me' }])
-  useEffect(() => {
-    if (messages[messages.length - 1].author == 'me') {
-      addMessage('Не пиши сюда больше', 'Robot')
-    }
-  }, [messages])
-  const addMessage = (message, author) => {
-    const id = messages[messages.length - 1].id+1
-    if (!!message) {
-      setMessages([...messages, { id: id, message: message, author: author }])
-    }
-  }
-  return (
-    <div className="App">
-      {messages.map(message => <Message key={messages.id} message={message} />)}
-      <NessageField addMessage={addMessage} />
-    </div>
-  );
+	const [messages, setMessages] = useState([
+		{ id: 0, message: 'dobri den', author: 'me' },
+		{ id: 1, message: 'Privet', author: 'me' },
+		{ id: 2, message: 'React curse', author: 'me' },
+	])
+
+	const prevMessage = usePrev(messages)
+	const addMessage = useCallback(
+		(message, author) => {
+			const id = messages[messages.length - 1].id + 1
+			if (!!message) {
+				setMessages([
+					...messages,
+					{ id: id, message: message, author: author },
+				])
+			}
+		},
+		[messages]
+	)
+
+	useEffect(() => {
+		if (
+			prevMessage?.length < messages.length &&
+			messages[messages.length - 1]?.author !== 'Robot'
+		) {
+			addMessage('Не пиши сюда больше', 'Robot')
+		}
+	}, [messages, addMessage, prevMessage])
+
+	return (
+		<div className='App'>
+			{messages.map((message) => (
+				<Message key={messages.id} message={message} />
+			))}
+			<NessageField addMessage={addMessage} />
+		</div>
+	)
 }
 
-export default App;
+export default App
