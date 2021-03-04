0e85fd067aed6dc79c33467a7e7fa6829093b70c
Rillai <62833808+Rillai@users.noreply.github.com>
2021-03-05 00:23:17 +0400

    Upp

--- src/App.js
+++ src/App.js
@@ -1,47 +1,39 @@
-import './App.css'
-import { useCallback, useEffect, useState } from 'react'
-import { Message } from './components/message'
-import { NessageField } from './components/messageField'
-import { usePrev } from './hooks/usePrev'
+import './App.css';
+import { useEffect, useState } from 'react';
+import { ChatField } from './components/chatField';
+import { Container } from '@material-ui/core';
+import { Header } from './components/header';
+import { ChatList } from './components/chatList';
+import { Message } from './components/message';
 
 const App = () => {
-	const [messages, setMessages] = useState([
-		{ id: 0, message: 'dobri den', author: 'me' },
-		{ id: 1, message: 'Privet', author: 'me' },
-		{ id: 2, message: 'React curse', author: 'me' },
-	])
+  const [messages, setMessages] = useState([{ id: 0, message: 'dobri den', author: 'me' }, { id: 1, message: 'Privet', author: 'me' }, { id: 2, message: 'React curse', author: 'me' }])
+  useEffect(() => {
+    if (messages[messages.length - 1].author == 'me') {
+      addMessage('Не пиши сюда больше', 'Robot')
+    }
+  }, [messages])
+  const addMessage = (message, author) => {
+    const id = messages[messages.length - 1].id + 1
+    if (!!message) {
+      setMessages([...messages, { id: id, message: message, author: author }])
+    }
+  }
+  return (
+    <div className="app">
+      <Header />
+      <Container>
+        <div className='app-content'>
+          <ChatList />
+          <div>
+            {messages.map(message => <Message key={message.id} message={message} />)}
+            <ChatField addMessage={addMessage} />
+          </div>
+        </div>
+      </Container>
 
-	const prevMessage = usePrev(messages)
-	const addMessage = useCallback(
-		(message, author) => {
-			const id = messages[messages.length - 1].id + 1
-			if (!!message) {
-				setMessages([
-					...messages,
-					{ id: id, message: message, author: author },
-				])
-			}
-		},
-		[messages]
-	)
-
-	useEffect(() => {
-		if (
-			prevMessage?.length < messages.length &&
-			messages[messages.length - 1]?.author !== 'Robot'
-		) {
-			addMessage('Не пиши сюда больше', 'Robot')
-		}
-	}, [messages, addMessage, prevMessage])
-
-	return (
-		<div className='App'>
-			{messages.map((message) => (
-				<Message key={messages.id} message={message} />
-			))}
-			<NessageField addMessage={addMessage} />
-		</div>
-	)
+    </div>
+  );
 }
 
-export default App
+export default App;
